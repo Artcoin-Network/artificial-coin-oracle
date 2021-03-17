3cc6da18c9ec840936540eb1f0e8bfeeacdd367d
Bo Yao <bo@nearprotocol.com>
2021-03-16 22:59:19 -0700

    oracle fully works

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-let Web3 = require('web3');
+let Web3 = require('web3')
+let yahooStockPrices = require('yahoo-stock-prices')
+let nearAPI = require('near-api-js')
+let { KeyPair } = nearAPI;
+let fetch = require('node-fetch')
+let getConfig = require('./config')
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 async function main() {
+    let { contract, nearConfig } = await initContract()
+
     while (true) {
         let prices;
         try {
@@ -18,7 +25,7 @@ async function main() {
         }
 
         try {
-            await submitPrices(prices)
+            await submitPrices(prices, contract)
         } catch (e) {
             console.error('error to submit price: ')
             console.error(e)
@@ -32,8 +39,23 @@ async function main() {
 
 let last_prices = {}
 
-async function submitPrices(prices) {
+function priceToContrat(price) {
+    return Math.floor(Number(price) * Math.pow(10, 8)).toString()
+}
 
+async function submitPrices(prices, contract) {
+    console.log('submit price')
+    console.log(prices)
+    let a = await contract.submit_price({ price: priceToContrat(prices['art']) })
+    console.log(a)
+    for (let k in prices) {
+        if (k != 'art') {
+            let name = 'a'+k.toUpperCase()
+        let a = await contract.submit_asset_price({asset:name, price:priceToContrat(prices[k])})
+            console.log(a)
+        }
+    }
+    console.log(a)
 }
 
 const chainLinkFeeds = {
@@ -61,6 +83,24 @@ async function getPriceFromChainLink(name) {
     return Number(data.answer)/Math.pow(10, chainLinkFeeds[name].multi)
 }
 
+async function getPriceFromYahooFinance(name) {
+    const data = await yahooStockPrices.getCurrentData(name);
+    console.log(data) // { currency: 'USD', price: 132.05 }
+    return data.price
+}
+
+// async function getCoinDayRateRecent(coin) {
+//     let req = await fetch(`https://api.coingecko.com/api/v3/coins/${coin}/market_chart?vs_currency=usd&days=91`);
+//     let resp = await req.json();
+//     return resp;
+// }
+
+async function getCoinPriceFromCoingecko(coin) {
+    let req = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`)
+    let resp = await req.json();
+    return resp[coin].usd
+}
+
 async function getPrices() {
     if (last_prices['art']) {
         last_art_price = last_prices['art']
@@ -82,6 +122,51 @@ async function getPrices() {
 
     let gold = await getPriceFromChainLink('GOLD')
     console.log('current aGOLD price: $', gold)
+
+    let spy = await getPriceFromYahooFinance('SPY')
+    console.log('current aSPY price: $', spy)
+
+    let near = await getCoinPriceFromCoingecko('near')
+    console.log('current aSPY price: $', near)
+
+    return {btc, eur, gold, spy, near, art: last_prices['art']}
 }
 
-main()
+async function initContract() {
+    let networkId = process.env.NODE_ENV || 'testnet'
+    console.log(networkId)
+    const nearConfig = getConfig(networkId);
+  
+    // Initializing connection to the NEAR TestNet
+    let keyStore = new nearAPI.keyStores.InMemoryKeyStore()
+    let fs = require('fs')
+    let kf = fs.readFileSync(nearConfig.contractName + '.json')
+    let k = JSON.parse(kf)
+    keyStore.setKey(networkId, nearConfig.contractName, KeyPair.fromString(k.private_key))
+    if (networkId == 'testnet')
+        keyStore.setKey('default', nearConfig.contractName, KeyPair.fromString(k.private_key))
+    const near = await nearAPI.connect({
+      deps: {
+        keyStore,
+        },
+      ...nearConfig
+    });
+
+  
+    let account = await near.account( nearConfig.contractName);
+
+    // Initializing our contract APIs by contract name and configuration
+    const contract = await new nearAPI.Contract(account, nearConfig.contractName, {
+      // View methods are read-only – they don't modify the state, but usually return some value
+      viewMethods: [],
+      // Change methods can modify the state, but you don't receive the returned value when called
+      changeMethods: ['submit_price', 'submit_asset_price'],
+      // Sender is the account ID to initialize transactions.
+      // getAccountId() will return empty string if user is still unauthorized
+      sender: nearConfig.contractName
+    })
+  
+    return { contract, nearConfig }
+}
+
+main() 
